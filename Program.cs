fcfcb19580402e12ce625977b2d0a4ccef8e7fb1
Tej Prakash <Tej Prakash@JTF-PC>
2021-03-03 15:39:30 +0530

    CommitAdd .gitignore and .gitattributes.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Arithmetic_Functions
+{
+    class Program
+    {
+        static int sum(int a, int b)
+        {
+            return a + b;
+        } 
+        static int sub(int a, int b)
+        {
+            return a - b;
+        }
+        static int star(int a, int b)
+        {
+            return a * b;
+        }
+        static int div(int a, int b)
+        {
+            return a / b;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter First Intger: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Second Intger: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Sum = {sum(a,b)} | Difference = {sub(a,b)} | Product = {star(a,b)} | Division = {div(a,b) }");
+        }
+    }
+}
